e19b8fba1622108d02585b9d61090c44c0ff0639
ZhangYao <zhangyao4182@gmail.com>
2026-04-23 16:13:20 +0800

    Update 10_ArrayMoreExer.c

--- chapter04/10_ArrayMoreExer.c
+++ chapter04/10_ArrayMoreExer.c
@@ -9,18 +9,20 @@
 #define COLS 4
 
 int main() {
-
     //练习1：获取所有元素的和
     //定义二维数组
-    int arr1[ROWS][COLS] = {{3,  5, 8},
-                            {12, 9},
-                            {7,  0, 6, 4}}; //未显式赋值的位置，自动会被赋值为0
+    int arr1[ROWS][COLS] = {
+        {3, 5, 8},
+        {12, 9},
+        {7, 0, 6, 4}
+    }; //未显式赋值的位置，自动会被赋值为0
 
     //遍历二维数组，获取每个元素值，累加到变量sum中
     int sum = 0; //记录所有元素的和
     int i, j;
     for (i = 0; i < ROWS; i++) {
         for (j = 0; j < COLS; j++) {
+            printf("%-4d", arr1[i][j]); // %-4d 让数字左对齐并占4位，看起来更整齐
             sum += arr1[i][j];
         }
     }
@@ -30,26 +32,27 @@ int main() {
 
 
     //练习2：求二维数组最大值以及对应的行列角标
-    int a[ROWS][COLS] = {{1,   2,  3,  4},
-                         {9,   8,  7,  6},
-                         {-10, 10, -5, 2}};
+    int a[ROWS][COLS] = {
+        {1, 2, 3, 4},
+        {9, 8, 7, 6},
+        {-10, 100, -5, 2}
+    };
     int max = a[0][0]; //记录最大值。初始值为首元素a[0][0]
     int maxRow = 0; //记录最大值对应的行角标
     int maxCol = 0; //记录最大值对应的列角标
 
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            if(max < a[i][j]){
+            if (max < a[i][j]) {
                 max = a[i][j];
                 maxRow = i;
                 maxCol = j;
             }
         }
     }
-    printf("最大值为%d\n",max);
-    printf("最大值对应的行角标为%d\n",maxRow);
-    printf("最大值对应的列角标为%d\n",maxCol); //row:行  column:列
-
+    printf("最大值为%d\n", max);
+    printf("最大值对应的行角标为%d\n", maxRow);
+    printf("最大值对应的列角标为%d\n", maxCol); //row:行  column:列
 
 
     return 0;

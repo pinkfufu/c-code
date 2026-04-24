185875728064a34d0f073fec2b054a1bb91255f3
ZhangYao <zhangyao4182@gmail.com>
2026-04-25 00:22:47 +0800

    高级指针

---
+++ chapter05/AA_AdvancedPointers.c
@@ -0,0 +1,18 @@
+//
+// Created by ZhangYao on 2026/4/24.
+//
+//高级指针
+
+#include <stdio.h>
+
+int main() {
+    // 解决 Windows 下控制台输出缓冲区导致的显示延迟问题
+    setbuf(stdout, NULL);
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *P = &arr[0];
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", *(P + i));
+    }
+
+    return 0;
+}

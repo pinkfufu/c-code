a5e4f7be00f26fa7ddd0a5ed8c48091b64fc616d
ZhangYao <zhangyao4182@gmail.com>
2026-05-10 00:54:54 +0800

    共同体

---
+++ chapter10/01_04_UnionTest.cpp
@@ -0,0 +1,23 @@
+//
+// Created by ZhangYao on 2026/5/9.
+//
+
+
+#include <stdio.h>
+
+/**
+ * 核心：一种数据可能有多种类型
+需求：
+金融项目中，钱有可能是整数，小数，字符串，请定义对应的共同体
+ */
+union monery {
+
+};
+
+int main() {
+    // 解决 Windows 下控制台输出缓冲区导致的显示延迟问题
+    setbuf(stdout, NULL);
+
+
+    return 0;
+}

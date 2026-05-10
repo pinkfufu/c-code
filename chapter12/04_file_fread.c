da166e0d32389fd1f2cc7011ab928713abbcf443
ZhangYao <zhangyao4182@gmail.com>
2026-05-11 00:11:57 +0800

    Update 04_file_fread.c

--- chapter12/04_file_fread.c
+++ chapter12/04_file_fread.c
@@ -11,6 +11,13 @@ fread(void *ptr, size_t size, size_t nmemb, FILE *stream) 按块读取。
 适合读取二进制数据（如图片、音频）或已知大小的文件，效率高。
 这里用二进制模式 "rb" 确保字节数准确。
  */
+
+/**
+ *Buffer        void*   内存缓冲区的地址，从文件中读到的数据就存到这里。这块内存必须由你自己事先分配好。
+ *ElementSize   size_t  每个元素的大小，单位是字节。比如读取 int 数组时，ElementSize = sizeof(int)（通常为4）。
+ *ElementCount	size_t	要读取的元素个数，不是总字节数。总读取字节数 = ElementSize × ElementCount。
+ *Stream	    FILE*	文件指针，由 fopen 返回的已打开文件的句柄。
+ */
 int main() {
     // 解决 Windows 下控制台输出缓冲区导致的显示延迟问题
     setbuf(stdout, NULL);
